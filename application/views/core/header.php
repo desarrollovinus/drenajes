352f6007cb706d9636b43b3a1a7f8d7b9f155ce3
johncanovinus <john.cano@vinus.com.co>
2016-07-05 09:45:58 -0500

    Inicio configurado
    - Controlador y vista iniciales configurado
    - Menú activado
    - Íconos superiores adicionados

---
+++ application/views/core/header.php
@@ -0,0 +1,39 @@
+<!-- Etiquetas meta -->
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<meta name="description" content="">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+
+<!-- Título -->
+<title><?php echo $titulo; ?> | Drenajes</title>
+
+<!-- Estilos bootstrap -->
+<link rel="stylesheet" href="<?php echo base_url(); ?>css/bootstrap.min.css">
+<link rel="stylesheet" href="<?php echo base_url(); ?>css/bootstrap-theme.min.css">
+
+<!-- Estilos semantic ui -->
+<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700|Open+Sans:300italic,400,300,700' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>semantic-ui/dist/semantic.css">
+
+<!-- Estilos fechas -->
+<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>css/datepicker/default.css">
+<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>css/datepicker/default.date.css">
+<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>css/datepicker/default.time.css">
+
+<!-- Estilos generales -->
+<link href="<?php echo base_url(); ?>css/dataTables.bootstrap.css" rel="stylesheet">
+<link rel="stylesheet" href="<?php echo base_url(); ?>css/estilos.min.css">
+
+<!-- Librerías -->
+<script src="<?php echo base_url(); ?>js/plugins/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="<?php echo base_url(); ?>js/plugins/jquery-1.11.2.min.js"><\/script>')</script>
+<script src="<?php echo base_url(); ?>js/plugins/numeric.min.js"></script>
+<script src="<?php echo base_url(); ?>js/plugins/dataTables/jquery.dataTables.js"></script>
+<script src="<?php echo base_url(); ?>js/plugins/dataTables/dataTables.bootstrap.js"></script>
+<script src="<?php echo base_url(); ?>js/plugins/highcharts/highcharts.js"></script>
+<script src="<?php echo base_url(); ?>js/plugins/highcharts/modules/exporting.js"></script>
+<script src="<?php echo base_url(); ?>js/plugins/datepicker/picker.js"></script>
+<script src="<?php echo base_url(); ?>js/plugins/datepicker/picker.time.js"></script>
+<script src="<?php echo base_url(); ?>js/plugins/datepicker/picker.date.js"></script>
+<script src="<?php echo base_url(); ?>js/plugins/datepicker/legacy.js"></script>
---
+++ application/views/core/menu.php
@@ -0,0 +1,81 @@
+<div id="menu_superior" class="ui menu">
+	<!-- Ícono menú -->
+    <a onclick="javascript:menu_lateral()" class="item">
+        <i class="large sidebar icon" ></i>
+    </a>
+	
+	<!-- Ícono atrás -->
+    <a id="icono_volver" onclick="javascript:volver()" class="item" data-content="Volver" data-variation="inverted">
+        <i class="large reply icon"></i>
+    </a>
+
+	<!-- Menú superior derecho -->
+    <div class="right menu">
+		<!-- Historial -->
+        <a id="icono_historial" class="item" onClick="javascript:historial();" data-content="Ver historial en la aplicación" data-variation="inverted">
+            <i class="unhide icon"></i>
+        </a>
+
+        <!-- Guardar -->
+        <a id="icono_guardar" class="item" onClick="javascript:guardar();" data-content="Guardar" data-variation="inverted">
+            <i class="save icon"></i>
+        </a>
+
+        <!-- Listar -->
+        <a id="icono_listar" class="item" onClick="javascript:listar();" data-content="Mostrar todos los registros" data-variation="inverted">
+            <i class="table icon"></i>
+        </a>
+
+        <!-- Agregar -->
+        <a id="icono_crear" class="item" onClick="javascript:crear();" data-content="Crear nuevo registro" data-variation="inverted">
+            <i class="plus icon"></i>
+        </a>
+
+        <!-- Editar -->
+        <a id="icono_editar" class="item" onClick="javascript:editar();" data-content="Editar registro" data-variation="inverted">
+            <i class="edit icon"></i>
+        </a>
+
+        <!-- Eliminar -->
+        <a id="icono_eliminar" class="item" onClick="javascript:eliminar('confirmacion');" data-content="Eliminar registro" data-variation="inverted">
+            <i class="trash icon"></i>
+        </a>
+
+        <!-- Generar PDF -->
+        <a id="icono_pdf" class="item" onClick="javascript:generar_reporte('pdf');" data-content="Generar PDF" data-variation="inverted">
+            <i class="file pdf outline icon"></i>
+        </a>
+	</div><!-- Menú superior derecho -->
+</div>
+
+<div id="menu_lateral" class="ui left vertical inverted labeled icon sidebar menu active">
+	<!-- Para el menú de cada aplicación -->
+    <?php // if ($menu != "general") { ?>
+        <!-- Inicio -->
+        <a class="item" onClick="javascript:cargar_principal('inicio')">
+            <i class="home icon"></i>
+            Inicio
+        </a>
+    <?php // } // if ?>
+
+	
+
+
+
+
+</div>
+
+<script type="text/javascript">
+	/**
+     * Carga por Ajax la interfaz seleccionada
+     */
+    function cargar_principal(tipo){
+        // Dependiendo del tipo
+        switch(tipo) {
+           
+        } // switch tipo
+
+        // Se quita el menú
+        menu_lateral();
+    } // switch
+</script>
